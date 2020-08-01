27b94e9cba53e8bb7c9dc1df1363cb4b32f7af99
Ali Abir Shuvro <44960637+aliabirshuvro@users.noreply.github.com>
2020-08-01 22:00:43 +0600

    Signup Implemented
    Needs work on back button and popups

--- src/org/iut_ehealth/Signup/SignupController.java
+++ src/org/iut_ehealth/Signup/SignupController.java
@@ -4,6 +4,8 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,13 +17,16 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.iut_ehealth.DatabaseConnection;
 import org.iut_ehealth.UserSession;
-
+import java.sql.PreparedStatement;
+import javafx.fxml.Initializable;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class SignupController {
+public class SignupController implements Initializable {
     @FXML
     private JFXTextField emailField = new JFXTextField();
     @FXML
@@ -51,16 +56,25 @@ public class SignupController {
     @FXML
     private JFXTextField contactnumberField = new JFXTextField();
     @FXML
-    private JFXComboBox ageField = new JFXComboBox();
+    private JFXComboBox<String> ageField = new JFXComboBox<>();
     @FXML
-    private JFXComboBox bloodgroupField = new JFXComboBox();
+    private JFXComboBox<String> bloodgroupField = new JFXComboBox<>();
     @FXML
-    private JFXComboBox departmentField = new JFXComboBox();
-
+    private JFXComboBox<String> departmentField = new JFXComboBox<>();
 
     UserSession userSession = UserSession.getInstance();
     DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
-
+    public void initialize(URL url, ResourceBundle rb){
+        departmentField.getItems().addAll("CSE","EEE","MPE","CEE","BTM","TVE");
+        bloodgroupField.getItems().addAll("A+","A-","B+","B-","AB+","AB-","O+","O-");
+        ageField.getItems().addAll("1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949",
+                "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959",
+                "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969",
+                "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979",
+                "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989",
+                "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999",
+                "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009");
+    }
     public void onStudentButtonChosen(ActionEvent actionEvent) {
         doctorButton.setSelected(false);
         teacherButton.setSelected(false);
@@ -93,94 +107,109 @@ public class SignupController {
             Connection myConn = databaseConnection.getConnectionObject();
             Statement myStatement = myConn.createStatement();
 
-            if(studentButton.isSelected()) {
-                String studentQuery = "select *from userstudent where studentid = '"+emailField.getText()+"'and studentpassword = '"+passwordField.getText()+"'";
-                ResultSet rs = myStatement.executeQuery(studentQuery);
-                if(rs.next()){
-//                    System.out.println(emailField.getText());
-//                    System.out.println(passwordField.getText());
-                    userSession.setUsername(emailField.getText());
-                    userSession.setUsertype("student");
-                    //the scene that we want to load
-                    Parent studentHomepage = FXMLLoader.load(getClass().getResource("../Student/StudentHomepage/studentHomepage.fxml"));
-                    Scene studentHomepageScene = new Scene(studentHomepage);
-
-
-                    //this line gets stage information
-                    Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-                    //login confirmation
-                    Stage dialog = new Stage();
-                    dialog.initOwner(window);
-                    dialog.setHeight(250);
-                    dialog.setWidth(500);
-                    Scene loginSuccess = new Scene(FXMLLoader.load(getClass().getResource("./loginSuccess.fxml")));
-                    dialog.setScene(loginSuccess);
-                    dialog.initModality(Modality.APPLICATION_MODAL);
-                    dialog.showAndWait();
-
-                    window.setScene(studentHomepageScene);
-                    window.show();
+            String stat,p,c;
+            p=String.valueOf(passwordField.getText());
+            c=String.valueOf(confirmpasswordField.getText());
+            int flag=0;
+            if(studentButton.isSelected()){
+                String sq2 ="select * from userstudent where studentid = '"+idField.getText()+"'";
+                ResultSet rss=myStatement.executeQuery(sq2);
+                if(rss.next()){
+                    //JOptionPane.showMessageDialog(null, "ID already signed up!");
+                    //signupform sf = new signupform();
+                    //dispose();
+                    flag=1;
                 }
-                else System.out.println("invalid username/password");
             }
-            else if(teacherButton.isSelected()) {
-                String teacherQuery = "select *from userteacher where teacherid = '"+emailField.getText()+"'and teacherpassword = '"+passwordField.getText()+"'";
-                ResultSet rs = myStatement.executeQuery(teacherQuery);
-                if(rs.next()){
-                    userSession.setUsername(emailField.getText());
-                    userSession.setUsertype("teacher");
-                    //the scene that we want to load
-                    Parent teacherHomepage = FXMLLoader.load(getClass().getResource("../Teacher/TeacherHomepage/teacherHomepage.fxml"));
-                    Scene teacherHomepageScene = new Scene(teacherHomepage);
-                    //this line gets stage information
-                    Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-                    //login confirmation
-                    Stage dialog = new Stage();
-                    dialog.initOwner(window);
-                    dialog.setHeight(250);
-                    dialog.setWidth(500);
-                    Scene loginSuccess = new Scene(FXMLLoader.load(getClass().getResource("./loginSuccess.fxml")));
-                    dialog.setScene(loginSuccess);
-                    dialog.initModality(Modality.APPLICATION_MODAL);
-                    dialog.showAndWait();
-
-                    window.setScene(teacherHomepageScene);
-                    window.show();
+            else if(teacherButton.isSelected()){
+                String sq2 ="select * from userteacher where teacherid = '"+idField.getText()+"'";
+                ResultSet rss=myStatement.executeQuery(sq2);
+                if(rss.next()){
+                    //JOptionPane.showMessageDialog(null, "ID already signed up!");
+                    //signupform sf = new signupform();
+                    //dispose();
+                    flag=1;
                 }
-                else System.out.println("invalid username/password");
             }
-            else if(doctorButton.isSelected()) {
-                String doctorQuery = "select *from userdoctor where doctorid = '"+emailField.getText()+"'and doctorpassword = '"+passwordField.getText()+"'";
-                ResultSet rs = myStatement.executeQuery(doctorQuery);
-                if(rs.next()){
-                    userSession.setUsername(emailField.getText());
-                    userSession.setUsertype("doctor");
-                    //the scene that we want to load
-                    Parent doctorHomepage = FXMLLoader.load(getClass().getResource("../Doctor/DoctorHomepage/doctorHomepage.fxml"));
-                    Scene doctorHomepageScene = new Scene(doctorHomepage);
-                    //this line gets stage information
-                    Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
-
-                    //login confirmation
-                    Stage dialog = new Stage();
-                    dialog.initOwner(window);
-                    dialog.setHeight(250);
-                    dialog.setWidth(500);
-                    Scene loginSuccess = new Scene(FXMLLoader.load(getClass().getResource("./loginSuccess.fxml")));
-                    dialog.setScene(loginSuccess);
-                    dialog.initModality(Modality.APPLICATION_MODAL);
-                    dialog.showAndWait();
-
-                    window.setScene(doctorHomepageScene);
-                    window.show();
+            else{
+                String sq2 ="select * from userdoctor where doctorid = '"+idField.getText()+"'";
+                ResultSet rss=myStatement.executeQuery(sq2);
+                if(rss.next()){
+                    //JOptionPane.showMessageDialog(null, "ID already signed up!");
+                    //signupform sf = new signupform();
+                    //dispose();
+                    flag=1;
                 }
-                else System.out.println("invalid username/password");
             }
-            else System.out.println("select a type of user");
+            if(p.equals(c)&&flag==0){
+                String sql,sql2;
+                if(studentButton.isSelected()){
+                    String sq2 ="select * from userstudent where studentid = '"+idField.getText()+"'";
+                    ResultSet rss=myStatement.executeQuery(sq2);
+                    if(rss.next()){
+                        //JOptionPane.showMessageDialog(null, "ID already signed up!");
+                        //signupform sf = new signupform();
+                        //dispose();
+                    }
+                    sql = "insert into userstudentinfo values (?,?,?,?,?,?,?,?,?,?)";
+                    sql2= "insert into userstudent values (?,?)";
+                }
+                else if(teacherButton.isSelected()){
+                    sql = "insert into userteacherinfo values (?,?,?,?,?,?,?,?,?,?)";
+                    sql2= "insert into userteacher values (?,?)";
+                }
+                else {
+                    sql = "insert into userdoctorinfo values (?,?,?,?,?,?,?,?,?,?)";
+                    sql2= "insert into userdoctor values (?,?)";
+                }
 
-            // System.out.println("Connection successful");
+
+                PreparedStatement pst = myConn.prepareStatement(sql);
+                pst.setString(1, nameField.getText());
+                pst.setString(2, (String)ageField.getValue());
+                pst.setString(3, (String)bloodgroupField.getValue());
+                pst.setString(4, idField.getText());
+                pst.setString(5, (String)departmentField.getValue());
+                pst.setString(6, contactnumberField.getText());
+                pst.setString(7, emailField.getText());
+                if(ResidentialButton.isSelected())
+                    stat="Residential";
+                else if(NonResButton.isSelected())
+                    stat="Nonres";
+                else
+                    stat="Nonapp";
+                pst.setString(8, stat);
+                pst.setString(9, addressField.getText());
+                pst.setString(10, null);
+                pst.execute();
+
+                PreparedStatement pst2=myConn.prepareStatement(sql2);
+                pst2.setString(1,idField.getText());
+                pst2.setString(2,p);
+                pst2.execute();
+
+                Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+                //login confirmation
+                Stage dialog = new Stage();
+                dialog.initOwner(window);
+                dialog.setHeight(250);
+                dialog.setWidth(500);
+                Scene loginSuccess = new Scene(FXMLLoader.load(getClass().getResource("./signupSuccess.fxml")));
+                dialog.setScene(loginSuccess);
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                dialog.showAndWait();
+                //signinform signinf = new signinform();
+                //signinf.setVisible(true);
+                //JOptionPane.showMessageDialog(null, "Signup Successful.");
+                //dispose();
+            }
+            else if(flag==1){
+                //JOptionPane.showMessageDialog(null, "Try logging in");
+            }
+            else{
+                //JOptionPane.showMessageDialog(null, "Password and Confirmation did not match.");
+
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
