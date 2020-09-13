b19f3fc1fa8c0bd8c00e82d168a91e6b151a1158
refaat31 <refaatalamgir@iut-dhaka.edu>
2020-09-14 00:49:49 +0600

    IUT Logo & Window Title

--- src/org/iut_ehealth/App.java
+++ src/org/iut_ehealth/App.java
@@ -4,11 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
-
-
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -29,6 +29,9 @@ public class App extends Application {
         stage.setWidth(1000);
         stage.setResizable(false);
 
+        Image iutLogo = new Image(new File("src/org/iut_ehealth/icons/iutLogo.png").toURI().toString());
+        stage.getIcons().add(iutLogo);
+        stage.setTitle("    IUT E-Health System");
         // only connect to db once
         DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
         databaseConnection.connectToDatabase();
